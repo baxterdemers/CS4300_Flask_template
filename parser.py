6a885a7405a0c9f97d4981d7e7932bf3a5942251
Sofie Cornelis <sacornelis97@gmail.com>
2019-04-19 14:06:44 -0400

    fix the named entity parser to get names together

--- parser.py
+++ parser.py
@@ -2,9 +2,15 @@ def parse_document(text):
     people = []
     tokens = nltk.tokenize.word_tokenize(text) #tokenize to remove punctuation
     token_to_pos = nltk.pos_tag(tokens) #returns list of tuples ([token, pos])
-    tagged_tree = nltk.ne_chunk(token_to_pos, binary = False) #tags named entities with PERSON, ORGANIZATION, GPE, etc, returns as a NLTK tree
-    for subtree in tagged_tree.subtrees(filter=lambda t: t.label() == 'PERSON'):
+    tagged_tree = nltk.ne_chunk(token_to_pos) #tags named entities with PERSON, ORGANIZATION, GPE, etc, returns as a NLTK tree
+#     print(tagged_tree)
+    for subtree in tagged_tree.subtrees(filter=lambda t: t.label() in ['PERSON', 'ORGANIZATION']):
+#         print(subtree)
+        entity = ""
         for leave in subtree.leaves():
-            people.append(leave[0])
+#             print(leave)
+            entity = entity + " " + leave[0]
+        people.append(entity.strip())  #strip to remove space before name
+#         print()
 
     return people
